8c175e94f3d8a87855c2c88ed93f67a06e945f74
26ANSH <anshvidyabhanu8@gmail.com>
2022-05-16 21:50:11 +0530

    Updated Templates

--- website/static/scripts/books/browse.js
+++ website/static/scripts/books/browse.js
@@ -4,15 +4,8 @@ function BtnDisable(value) {
     document.getElementById("bookBtnTwo").disabled = value
 }
 
-function getBookID(elt){
-    while (elt && elt.tagName != "ARTICLE")
-        elt = elt.parentNode
-
-    return elt.id;
-}
-
-function viewBook(elt) {
-    window.location.href = "/books/" + getBookID(elt)
+function viewBook(id) {
+    window.location.href = "/books/" + id
 }
 
 function getBooks(change) {
@@ -29,22 +22,25 @@ function getBooks(change) {
                     'Content-Type': 'application/json'
                 }
             }).then(function (response) {
-                return response.json();
+                if (response.ok)
+                    return response.json();
+                else
+                    throw response.json();
+
             }).then(function (data) {
                 data = data.items;
-                console.log(data)
 
                 if (data.length > 0) {
                     document.getElementById("loader").style.display = "none"
                     for (var i = 0; i < data.length; i++) {
 
-                    book = data[i]
+                        book = data[i]
 
-                    if(!book.volumeInfo.imageLinks){
-                        book.volumeInfo.imageLinks = { "thumbnail":"https://via.placeholder.com/150"}
-                    }
+                        if (!book.volumeInfo.imageLinks) {
+                            book.volumeInfo.imageLinks = { "thumbnail": "https://via.placeholder.com/150" }
+                        }
 
-                    document.getElementById("booksContent").innerHTML += `<article id="` + book.id + `" class="w-full justify-evenly flex flex-col shadow-lg resources-responsive m-1.5 border-2 hover:bg-blue-50 border-blue-900 p-2 rounded-md transition-linear duration-300">
+                        document.getElementById("booksContent").innerHTML += `<article id="` + book.id + `" class="w-full justify-evenly flex flex-col shadow-lg resources-responsive m-1.5 border-2 hover:bg-blue-50 border-blue-900 p-2 rounded-md transition-linear duration-300">
 
                     <h1 class="text-xl text-blue-800 font-bold"> `+ book.volumeInfo.title + `</h1>
             
@@ -53,8 +49,8 @@ function getBooks(change) {
                     <img src="`+ book.volumeInfo.imageLinks.thumbnail + `" class="mx-auto h-48" alt="">
 
                     <h3 class="self-end text-white my-1 mt-3 flex justify-evenly w-full">
-                    <button class="py-0.5 px-4 rounded-full bg-blue-700 hover:bg-blue-900" onclick="addtofav(this)">Add ❤️</button>
-                    <button class="py-0.5 px-4 rounded-full bg-blue-700 hover:bg-blue-900" onclick="viewBook(this)">View 👀</button>
+                    <button class="py-0.5 px-4 rounded-full bg-blue-700 hover:bg-blue-900" onclick="addToFavs('${book.id}')">Add ❤️</button>
+                    <button class="py-0.5 px-4 rounded-full bg-blue-700 hover:bg-blue-900" onclick="viewBook('${book.id}')">View 👀</button>
                     </h3>
                     
                     </article>`
@@ -68,8 +64,18 @@ function getBooks(change) {
                     BtnDisable(true)
                     document.getElementById("bookBtnOne").disabled = false
                 }
+            }).catch(function (error) {
 
+                const p = Promise.resolve(error)
+                p.then(function (data) {
+                    document.getElementById("booksContent").innerHTML += `<h1 class="text-xl text-red-700 mt-2 font-bold">${data.error}</h1>`
+                })
+
+                document.getElementById("loader").style.display = "none"
+                BtnDisable(true)
+                document.getElementById("bookBtnOne").disabled = false
             });
+
     }
 }
 
